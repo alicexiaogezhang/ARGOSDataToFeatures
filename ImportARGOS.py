8f312cab9ffe993fe32ab1483833b85f7111dc00
Xiaoge Zhang 张宵鸽 <Alice.xiaoge.zhang@gmail.com>
2022-10-22 19:04:31 -0400

    setup

---
+++ ImportARGOS.py
@@ -0,0 +1,22 @@
+##---------------------------------------------------------------------
+## ImportARGOS.py
+##
+## Description: Read in ARGOS formatted tracking data and create a line
+##    feature class from the [filtered] tracking points
+##
+## Usage: ImportArgos <ARGOS folder> <Output feature class> 
+##
+## Created: Fall 2020
+## Author: John.Fay@duke.edu (for ENV859)
+## 
+## Modified by: Xiaoge Zhang 
+## xiaoge.zhang@duke.edu
+##---------------------------------------------------------------------
+
+# Import modules
+import sys, os, arcpy
+
+# Set input variables (Hard-wired)
+inputFile = 'V:/ARGOSTracking/Data/ARGOSData/1997dg.txt'
+outputFC = "V:/ARGOSTracking/Scratch/ARGOStrack.shp"
+
